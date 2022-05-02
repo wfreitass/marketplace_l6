0898c028d46b91c758ea208480fb7bb58a7bcd50
Wiltter Freitas <wiltterfreitasw@gmail.com>
2022-05-02 15:20:10 -0300

    modulo 10 inicio

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -30,6 +30,11 @@
                             @endif">
                         <a class="nav-link" href="{{route("admin.products.index")}}">Produtos</a>
                         </li>
+                        <li class="nav-item @if (request()->is('admin/categories*'))
+                            active
+                            @endif">
+                        <a class="nav-link" href="{{route("admin.categories.index")}}">Categorias</a>
+                        </li>
                     </ul>
 
                   <div class=" my-2 my-lg-0">
